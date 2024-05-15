2ec6988312b73a4d0bc2f497019a0aaa65261e9c
Hampus Vigstedt <112432547+HampusWigstedt@users.noreply.github.com>
2024-05-15 21:27:17 +0200

    Update auth.js

--- app/utils/auth.js
+++ app/utils/auth.js
@@ -2,7 +2,7 @@
 import queryString from 'query-string';
 
 const CLIENT_ID = process.env.NEXT_PUBLIC_OWNER_SPOTIFY_CLIENT_ID;
-const REDIRECT_URI = 'http://localhost:3000/callback';
+const REDIRECT_URI = 'https://musiccurator.netlify.app/callback';
 const SCOPES = ['user-read-private', 'user-read-email', 'user-top-read']; // Add any additional scopes you need
 
 const authorizeSpotify = () => {
