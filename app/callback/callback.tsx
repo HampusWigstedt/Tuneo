113a759d8c9e4d39977547198855a099a680c6c6
Hampus Vigstedt <112432547+HampusWigstedt@users.noreply.github.com>
2024-05-15 21:26:57 +0200

    Update callback.tsx

--- app/callback/callback.tsx
+++ app/callback/callback.tsx
@@ -32,7 +32,7 @@ const CallbackPage = () => {
         const authToken = Buffer.from(`${clientId}:${clientSecret}`, 'utf-8').toString('base64');
         const data = qs.stringify({
           code: code,
-          redirect_uri: 'http://localhost:3000/callback',
+          redirect_uri: 'https://musiccurator.netlify.app/callback',
           grant_type: 'authorization_code'
         });
 
