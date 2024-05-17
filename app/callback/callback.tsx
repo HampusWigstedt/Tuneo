cb86e5e5e3d32c8fbf8c3062f9a5ff4476ff62f3
Hampus Vigstedt <112432547+HampusWigstedt@users.noreply.github.com>
2024-05-17 15:32:32 +0200

    Update callback.tsx

--- app/callback/callback.tsx
+++ app/callback/callback.tsx
@@ -32,7 +32,7 @@ const CallbackPage = () => {
         const authToken = Buffer.from(`${clientId}:${clientSecret}`, 'utf-8').toString('base64');
         const data = qs.stringify({
           code: code,
-          redirect_uri: 'https://musiccurator.netlify.app/callback',
+          redirect_uri: 'https://tuneo.netlify.app/callback',
           grant_type: 'authorization_code'
         });
 
