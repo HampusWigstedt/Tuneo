77d57de52b5505e4b97c7f93a747e018fca25090
Hampus Vigstedt <112432547+HampusWigstedt@users.noreply.github.com>
2024-05-17 15:26:20 +0200

    Update NavBar.tsx

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Link from 'next/link';
 import CheckLoggedIn from './isLoggedIn';
-import Icon from './icon'
+
 
 
 const NavBar = () => {
@@ -12,7 +12,11 @@ const NavBar = () => {
         <nav>
             <div className="navbar bg-primary text-primary-content">
                 <div className="flex-1 gap-3">
-                    <Icon />
+                    <Link href="/">
+                        <button className="">
+                            <img src="/favicon.ico" alt="Icon" />
+                        </button>
+                    </Link>
                     <div className="hidden md:flex gap-2">
                         <Link href="/topArtists" className="btn btn-primary text-xl hover:btn-neutral border-black border-2">Top Artists</Link>
                     </div>
