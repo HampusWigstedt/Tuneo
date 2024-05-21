cfba2081e1d1a703c6b27425683c0946b12f0388
Hampus Vigstedt <112432547+HampusWigstedt@users.noreply.github.com>
2024-05-21 11:39:47 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main>
       <div className='text-center p-96'>
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Get the AUX every time with <span className="text-green-600 dark:text-green-500">Tuneo</span></h1>
-        <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at MusicFinder, our goal is to give you the best experience when finding new music. We give you the tools to expand your music library</p>
+        <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Tuneo, our goal is to give you the best experience when finding new music. We give you the tools to expand your music library</p>
       </div>
     </main>
   )
