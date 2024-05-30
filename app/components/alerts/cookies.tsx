da3fde77cb03a75d297f6e00ebd301194be96d5f
Hampus Vigstedt <112432547+HampusWigstedt@users.noreply.github.com>
2024-05-30 12:31:29 +0200

    Update cookies.tsx

--- app/components/alerts/cookies.tsx
+++ app/components/alerts/cookies.tsx
@@ -1,49 +1,34 @@
 'use client'
-import React from 'react';
-import { useEffect, useState } from "react";
+import React from 'react'
+import { useEffect, useState } from "react"
 
 const Cookies = () => {
-    const [showAlert, setShowAlert] = useState(false);
+    const [showAlert, setShowAlert] = useState(false)
 
     useEffect(() => {
         if (!document.cookie.split(';').some((item) => item.trim().startsWith('cookieConsent='))) {
-            // The cookie does not exist, so show the cookie consent banner
-            setShowAlert(true);
+            setShowAlert(true)
         }
-    }, []);
+    }, [])
 
-    const handleDeny = () => {
-        window.location.href = '/error'
-    }
-
-    const handleAccept = () => {
-        // When the user gives their consent, set the cookieConsent cookie
-        // This cookie will expire in 365 days
-        document.cookie = 'cookieConsent=true; expires=' + new Date(new Date().getTime() + 365 * 24 * 60 * 60 * 1000).toUTCString();
-        window.location.href = '/'
-        setShowAlert(false);
+    const handleAcknowledge = () => {
+        document.cookie = 'cookieConsent=true; expires=' + new Date(new Date().getTime() + 365 * 24 * 60 * 60 * 1000).toUTCString()
+        setShowAlert(false)
     }
 
     return (
-<div>
-    {showAlert && (
-        <div className="card w-auto bg-base-content text-2xl underline shadow-xl my-6 text-black">
-            <div className="card-body text-center">
-                <div className="card-actions justify-end">
-                    <button className="btn btn-square btn-sm" onClick={handleDeny}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
+        <div>
+            {showAlert && (
+                <div className="card w-auto bg-base-content text-2xl underline shadow-xl my-6 text-black">
+                    <div className="card-body text-center">
+                        <p>We use essential cookies on our site. We only use these cookies for the application to operate accordingly.</p>
+                        <div className="flex justify-center">
+                            <button onClick={handleAcknowledge} className="btn btn-md btn-primary">Acknowledge</button>
+                        </div>
+                    </div>
                 </div>
-                <p>Cookies are essential for the application to work. If you do not accept you can not proceed. Please click here to accept cookies.</p>
-                <div className="flex justify-center">
-                    <button onClick={handleAccept} className="btn btn-md btn-primary">Accept</button>
-                </div>
-            </div>
+            )}
         </div>
-    )}
-</div>
     )
 }
 
