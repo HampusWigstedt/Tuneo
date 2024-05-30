3c92677558fe947a709576bcee816ad3c44b44db
Hampus Vigstedt <112432547+HampusWigstedt@users.noreply.github.com>
2024-05-30 12:30:54 +0200

    Update findNewMusic.tsx

--- app/findNewMusic/findNewMusic.tsx
+++ app/findNewMusic/findNewMusic.tsx
@@ -1,45 +1,47 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Cookies from 'js-cookie';
+'use client'
+import React, { useState, useEffect } from 'react' // Importing necessary dependencies
+import axios from 'axios' // Axios for making HTTP requests
+import Cookies from 'js-cookie' // js-cookie for handling cookies
 
 // Define an interface for the track object
 interface Track {
-    id: string;
-    name: string;
-    artists: { id: string; name: string }[];
-    image: { height: number; width: number; url: string };
-    external_urls: { spotify: string };
+    id: string
+    name: string
+    artists: { id: string; name: string }[]
+    image: { height: number; width: number; url: string }
+    external_urls: { spotify: string }
 }
 
 const Search = () => {
-    const [query, setQuery] = useState('');
-    const [searchResults, setSearchResults] = useState<Track[]>([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [recommendations, setRecommendations] = useState<Track[]>([]);
+    // State variables for managing the search query, search results, loading state, selected track, user authentication status, and track recommendations
+    const [query, setQuery] = useState('')
+    const [searchResults, setSearchResults] = useState<Track[]>([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [recommendations, setRecommendations] = useState<Track[]>([])
+    const [selectedTrack, setSelectedTrack] = useState<Track | null>(null)
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-
-    const [selectedTrack, setSelectedTrack] = useState<Track | null>(null);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+    // Function to check if the user is authenticated by looking for an 'access_token' cookie
     const checkAuthentication = () => {
-        const cookieInititated = Cookies.get('access_token');
+        const cookieInititated = Cookies.get('access_token')
         if (cookieInititated) {
-            setIsLoggedIn(true);
+            setIsLoggedIn(true)
         } else {
-            setIsLoggedIn(false);
+            setIsLoggedIn(false)
         }
-    };
+    }
 
+    // useEffect hook to call checkAuthentication when the component mounts
     useEffect(() => {
-        checkAuthentication();
-    }, []);
+        checkAuthentication()
+    }, [])
 
+    // Function to handle the selection of a track. This function makes an API call to Spotify's recommendations endpoint to get track recommendations based on the selected track
     const handleTrackSelection = async (track: Track) => {
-        setIsLoading(true);
-        setSelectedTrack(track);
+        setIsLoading(true)
+        setSelectedTrack(track)
 
-        const accessToken = Cookies.get('access_token');
+        const accessToken = Cookies.get('access_token')
 
         try {
             const response = await axios.get('https://api.spotify.com/v1/recommendations', {
@@ -50,7 +52,7 @@ const Search = () => {
                 headers: {
                     'Authorization': `Bearer ${accessToken}`,
                 },
-            });
+            })
 
             // Extract necessary data and set recommendations
             const recommendations: Track[] = response.data.tracks.map((track: any) => ({
@@ -59,23 +61,26 @@ const Search = () => {
                 artists: track.artists.map((artist: any) => ({ id: artist.id, name: artist.name })),
                 image: track.album.images[0], // Get the first image from the album
                 external_urls: track.external_urls // Get the external_urls object
-            }));
+            }))
 
             // Set the recommendations state
-            setRecommendations(recommendations);
+            setRecommendations(recommendations)
 
         } catch (error) {
-            console.error('Error getting recommendations:', error);
+            Cookies.remove('access_token')
+            window.location.href = '/'
+            console.error('Error getting recommendations:', error)
         } finally {
-            setIsLoading(false); // Set loading to false after the API call
+            setIsLoading(false) // Set loading to false after the API call
         }
 
-    };
+    }
 
+    // useEffect hook to search for tracks whenever the search query changes. This function makes an API call to Spotify's search endpoint
     useEffect(() => {
         const searchTracks = async () => {
-            setIsLoading(true);
-            const accessToken = Cookies.get('access_token');
+            setIsLoading(true)
+            const accessToken = Cookies.get('access_token')
 
             try {
                 const response = await axios.get('https://api.spotify.com/v1/search', {
@@ -87,7 +92,7 @@ const Search = () => {
                     headers: {
                         'Authorization': `Bearer ${accessToken}`,
                     },
-                });
+                })
 
                 // Extract necessary data and set search results
                 const tracks: Track[] = response.data.tracks.items.map((track: any) => ({
@@ -96,46 +101,49 @@ const Search = () => {
                     artists: track.artists.map((artist: any) => ({ id: artist.id, name: artist.name })),
                     image: track.album.images[0], // Get the first image from the album
                     external_urls: track.external_urls // Get the external_urls object
-                }));
-                setSearchResults(tracks);
+                }))
+                setSearchResults(tracks)
             } catch (error) {
-                console.error('Error searching tracks:', error);
+                console.error('Error searching tracks:', error)
             } finally {
-                setIsLoading(false); // Set loading to false after the API call
+                setIsLoading(false) // Set loading to false after the API call
             }
-        };
+        }
 
         if (query !== '') {
-            searchTracks();
+            searchTracks()
         }
-    }, [query]);
+    }, [query])
 
+    // Function to handle the search form submission. This function checks if the search query contains code-like patterns and alerts the user if it does. Otherwise, it sets the search query state which triggers the search
     const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const query = e.currentTarget.query.value;
+        e.preventDefault()
+        const query = e.currentTarget.query.value
 
         // Regular expression to match code-like patterns
         const codePattern = /<[^>]*>|{[^}]*}|function\s*\(|console\.log\(|import\s+|export\s+/;
 
         if (codePattern.test(query)) {
-            alert('Code input is not allowed.');
+            alert('Code input is not allowed.')
         } else {
             // Trigger the API call when the user submits the search query
-            setSearchResults([]); // Clear previous search results
-            setQuery(query);
+            setSearchResults([]) // Clear previous search results
+            setQuery(query)
         }
-    };
+    }
 
-
+    // If the component is in a loading state, return a loading message
     if (isLoading) {
         return (
-            <div className='text-center p-96'><h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Data is <span className="text-green-600 dark:text-green-500">Loading</span></h1>
-                <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Stay put. This could take a few secounds.</p>
+            <div className='text-center p-6 md:p-12 lg:p-24 xl:p-96'>
+                <h1 className="mb-4 text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white">Data is <span className="text-green-600 dark:text-green-500">Loading</span></h1>
+                <p className="text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-normal text-gray-500 dark:text-gray-400">Stay put. This could take a few secounds.</p>
                 <span className="loading loading-infinity loading-lg"></span>
             </div>
         )
     }
 
+    // Render the search form, search results, selected track, and track recommendations
     return (
         <div className="p-4 ">
             {!isLoggedIn ? (
@@ -144,11 +152,10 @@ const Search = () => {
                 </div>
             ) : (
                 <div>
-                    <div className='text-center text-4xl font-bold p-6'>
+                    <div className='text-center text-4xl font-bold p-10'>
                         <h1 className='p-4'>Search for a song</h1>
                         <form onSubmit={handleSearch}>
-                            <div className="tooltip tooltip-open tooltip-left" data-tip="First search for a Track or Artist here">
-                            <input type="text" name="query" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                            <div className="tooltip md:tooltip-open lg:tooltip-left sm:tooltip-closed md:tooltip-top" data-tip="First search for a Track or Artist here">                                <input type="text" name="query" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
                             </div>
                             <button type="submit" className="p-4">Search</button>
                         </form>
@@ -156,7 +163,7 @@ const Search = () => {
                     <div className="dropdown mt-2 text-center w-full top-full text-4xl font-bold p-6 flex justify-center">
                         <div className="relative">
                             {searchResults.length > 0 && (
-                                <div className="tooltip tooltip-open" data-tip="Now klick here to select a song">
+                                <div className="tooltip tooltip-open" data-tip="Now click here to select a song">
                                     <div tabIndex={0} role="button" className="btn btn-lg m-1 w-96">Select a track</div>
                                 </div>
                             )}
@@ -172,46 +179,47 @@ const Search = () => {
 
                     <div>
                         {/* Selected track display */}
-                        <div className="hero min-h-[200px] bg-base-200 p-10 mb-8 rounded-lg">
-                            <div className="hero-content flex-col lg:flex-row">
+                        <div className="hero min-h-[150px] bg-base-200 p-2 md:p-10 mb-8 rounded-lg overflow-auto">
+
                                 {selectedTrack && (
-                                    <div className="flex items-center space-x-4 justify-center my-6">
+                                    <div className="flex flex-col md:flex-row items-center space-x-4 justify-center my-6">
                                         <img src={selectedTrack.image?.url} className="max-w-xs rounded-lg shadow-2xl" alt={selectedTrack.name} />
                                         <div>
-                                            <h2 className="text-2xl font-bold underline text-emerald-900">You Selected</h2>
-                                            <h1 className="text-2xl font-bold">{selectedTrack.name}</h1>
+                                            <h2 className="md:text-2xl text-lg font-bold underline text-emerald-900">You Selected</h2>
+                                            <h1 className="md:text-2xl text-lg font-bold">{selectedTrack.name}</h1>
                                             <p className="py-2">{selectedTrack.artists.map(artist => artist.name).join(', ')}</p>
                                             <a href={selectedTrack.external_urls?.spotify} target="_blank" rel="noopener noreferrer" className="btn btn-primary">View on Spotify</a>
                                             <iframe className='rounded-2xl shadow-lg border-0 mt-6' src={`https://open.spotify.com/embed/track/${selectedTrack.id}`} width="300" height="80" allow="encrypted-media"></iframe>
                                         </div>
                                     </div>
                                 )}
-                            </div>
                         </div>
 
-                        <div className="grid grid-cols-4 gap-9 mx-20 pb-14">
-                            {recommendations.map((track, index) => (
-                                <div key={track.id} className="card w-96 bg-neutral">
-                                    <figure className='pt-4'>
-                                        <img className="object-cover p-2 rounded-3xl w-[320px] h-[320px]" src={track.image?.url} alt={track.name} />
-                                    </figure>
-                                    <div className="card-body">
-                                        <h2 className="card-title line-clamp-1">
-                                            {index + 1}. {track.name}
-                                        </h2>
-                                        <h4 className="card-side line-clamp-1">
-                                            {track.artists.map(artist => artist.name).join(', ')}
-                                        </h4>
-                                        <iframe className='rounded-2xl shadow-lg border-0 mt-6' src={`https://open.spotify.com/embed/track/${track.id}`} width="300" height="80" allow="encrypted-media"></iframe>
+                        <div className='flex justify-center items-center'>
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 py-6">
+                                {recommendations.map((track, index) => (
+                                    <div key={track.id} className="card w-full sm:w-96 bg-neutral">
+                                        <figure className='p-6'>
+                                            <img className="object-cover p-2 rounded-3xl w-full h-full" src={track.image?.url} alt={track.name} />
+                                        </figure>
+                                        <div className="card-body">
+                                            <h2 className="card-title line-clamp-1">
+                                                {index + 1}. {track.name}
+                                            </h2>
+                                            <h4 className="card-side line-clamp-1">
+                                                {track.artists.map(artist => artist.name).join(', ')}
+                                            </h4>
+                                            <iframe className='rounded-2xl shadow-lg border-0 mt-6' src={`https://open.spotify.com/embed/track/${track.id}`} width="300" height="80" allow="encrypted-media"></iframe>
+                                        </div>
                                     </div>
-                                </div>
-                            ))}
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
             )}
         </div>
-    );
-};
+    )
+}
 
-export default Search;
+export default Search
