ba0c11996d6d83cdca905a81e52a1bdf265a9072
Hampus Vigstedt <112432547+HampusWigstedt@users.noreply.github.com>
2024-10-16 00:52:44 +0200

    Update NavBar.tsx

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import Link from 'next/link'
 import CheckLoggedIn from './isLoggedIn'
-import Icon from './icon'
 import { useState } from 'react'
 
 const NavBar = () => {
