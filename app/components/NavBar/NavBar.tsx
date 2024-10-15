c7e3c98180de2f29562429383514a4fd04c53cc5
Hampus Vigstedt <112432547+HampusWigstedt@users.noreply.github.com>
2024-10-16 00:47:06 +0200

    Update NavBar.tsx

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Link from 'next/link'
 import CheckLoggedIn from './isLoggedIn'
+import Icon from './icon'
 import { useState } from 'react'
 
 const NavBar = () => {
@@ -12,16 +13,29 @@ const NavBar = () => {
     };
 
     return (
-        <nav>
-            <div className="navbar bg-primary text-primary-content">
-                <div className="flex-1 gap-3">
-                            <Link href="/">
-            <button className="w-10 h-10 md:w-12 md:h-12 lg:w-12 lg:h-12">
-                <img className="w-full h-full" src="/favicon.ico" alt="Icon" />
-            </button>
-        </Link>
+        <nav className="bg-base-300 shadow-lg p-6 my-2 rounded-full relative z-20 max-w-8xl mt-4 mx-auto">
+            <div className="flex justify-between items-center">
+                <div className="flex items-center gap-4">
+                    <Icon />
+                    <div className="hidden md:flex gap-4">
+                        <Link href="/topArtists">
+                            <span className="btn bg-violet-900 text-lg md:text-xl hover:btn-neutral">Top Artists</span>
+                        </Link>
+                        <Link href="/topTracks">
+                            <span className="btn bg-violet-900 text-lg md:text-xl hover:btn-neutral">Top Tracks</span>
+                        </Link>
+                        <Link href="/findNewMusic">
+                            <span className="btn bg-violet-900 text-lg md:text-xl hover:btn-neutral">Find New Music</span>
+                        </Link>
+                        <Link href="/ourPersonalChoice">
+                            <span className="btn bg-violet-900 text-lg md:text-xl hover:btn-neutral">Our Music Curation</span>
+                        </Link>
+                        <Link href="/blog">
+                            <span className="btn bg-violet-900 text-lg md:text-xl hover:btn-neutral">Blog</span>
+                        </Link>
+                    </div>
                     <div className="relative md:hidden">
-                        <button onClick={toggleMenu} className="btn btn-primary text-lg md:text-xl lg:text-2xl hover:btn-neutral border-black border-2 dropdown-toggle">Menu</button>
+                        <button onClick={toggleMenu} className="btn bg-violet-900 text-sm md:text-lg lg:text-xl hover:btn-neutral border-black border-2 dropdown-toggle">Menu</button>
                         {isOpen && (
                             <ul className="menu dropdown-content z-[1] p-2 shadow bg-primary rounded-box w-52 mt-4 absolute">
                                 <li>
@@ -52,31 +66,12 @@ const NavBar = () => {
                             </ul>
                         )}
                     </div>
-                    <div className="hidden md:flex gap-2">
-                        <Link href="/topArtists">
-                            <span className="btn btn-primary text-lg md:text-xl hover:btn-neutral border-black border-2">Top Artists</span>
-                        </Link>
-                        <Link href="/topTracks">
-                            <span className="btn btn-primary text-lg md:text-xl hover:btn-neutral border-black border-2">Top Tracks</span>
-                        </Link>
-                        <Link href="/findNewMusic">
-                            <span className="btn btn-primary text-lg md:text-xl hover:btn-neutral border-black border-2">Find New Music</span>
-                        </Link>
-                    </div>
                 </div>
-
-                <div className="flex-none gap-2">
-                    <div className="dropdown dropdown-end hidden md:block">
-                        <div tabIndex={0} role="button" className="btn btn-accent text-lg md:text-xl hover:btn-neutral border-black border-2">Our Music Curation</div>
-                        <ul tabIndex={0} className="menu dropdown-content z-[1] p-2 shadow bg-primary rounded-box w-52 mt-4">
-                            <li><Link href="/ourPersonalChoice"><span>Find New Music</span></Link></li>
-                            <li><Link href="/blog"><span>Blog</span></Link></li>
-                        </ul>
-                    </div>
+                <div className="flex items-center gap-6 px-4">
                     <CheckLoggedIn />
                 </div>
             </div>
-        </nav >
+        </nav>
     );
 };
 
